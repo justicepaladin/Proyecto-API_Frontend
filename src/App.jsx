239171d7712ec6748e2cf20d0a07b0dccef8f3b3
Facundo Martín Mayordomo <85538820+Facu-May@users.noreply.github.com>
2024-10-13 19:26:51 -0300

    Sección Nav
    Creación de sección Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Navigation from './Navigation/Nav'
+import './App.css';
+import Navigation from './Navigation/Nav';
+import Products from './Products/Products';
+import Recommended from './Recommended/Recommended';
 
 function App() {
   return (
     <>
       {/* <h1 className="text-center mt-4">API - 2024</h1> */}
       <Navigation />
+      <Products />
+      <Recommended />
     </>
-  )
+  );
 }
 
 export default App;
--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,8 +1,30 @@
 import "./Nav.css"; // Estilizado del Nav
+//icons
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import { FiHeart } from "react-icons/fi";
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+        <div className="nav-container">
+            <input 
+                type="text" 
+                className="search-input"
+                placeholder="Buscar" 
+            />
+        </div>
+        <div className="profile-container">
+            <a href="a">
+                <FiHeart className="nav-icons" />
+            </a>
+            <a href="a">
+                <AiOutlineShoppingCart className="nav-icons" />
+            </a>
+            <a href="a">
+                <AiOutlineUserAdd className="nav-icons" />
+            </a>
+        </div>
+    </nav>
   )
 }
 
