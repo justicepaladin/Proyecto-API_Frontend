25c9d62bcf31bb01cd47211bbde9676e7829369a
Mateo Nahuel Caré <mcare@uade.edu.ar>
2024-10-20 18:09:45 -0300

    Adición del primer producto
    Se agrega el primer producto para la sección de productos en general, luego con esto se podrá realizar una sección de favoritos y una sección de "recomendados"

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,9 +1,24 @@
+import {AiFillStar} from "react-icons/ai";
+
 import "./Products.css";
 
 function Products() {
-  return (
-    <div>Products</div>
-  )
+  return <>
+    <section className="card-container">
+      <section className="card">
+        <img src="https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg" alt="Shoe"/>
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar/>
+          </section>
+        </div>
+      </section>
+
+    </section>
+
+
+  </>
 }
 
 export default Products;
