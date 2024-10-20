8ac3281aa874b0f6861de5fe0b608dba2d0110da
Mateo Nahuel Caré <mcare@uade.edu.ar>
2024-10-20 19:22:32 -0300

    Modificación de "detalles"
    Se agregan y modifican algunos detalles para que el contenido de los productos sea mas "agradable a la vista"

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,4 +1,5 @@
 import {AiFillStar} from "react-icons/ai";
+import { IoBagAddSharp } from "react-icons/io5";
 
 import "./Products.css";
 
@@ -6,11 +7,99 @@ function Products() {
   return <>
     <section className="card-container">
       <section className="card">
-        <img src="https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg" alt="Shoe"/>
+        <img src="https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg"
+             alt="Shoe"
+             className="card-img"/>
         <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
+          <h3 className="card-title">Zapatilla</h3>
           <section className="card-reviews">
-            <AiFillStar/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <span className="total-reviews"> 4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>
+              $200
+            </div>
+            <div className="bag">
+              <IoBagAddSharp className="bag-icon"/>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <section className="card">
+        <img src="https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg"
+             alt="Shoe"
+             className="card-img"/>
+        <div className="card-details">
+          <h3 className="card-title">Zapatilla</h3>
+          <section className="card-reviews">
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <span className="total-reviews"> 4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>
+              $200
+            </div>
+            <div className="bag">
+              <IoBagAddSharp className="bag-icon"/>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <section className="card">
+        <img src="https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg"
+             alt="Shoe"
+             className="card-img"/>
+        <div className="card-details">
+          <h3 className="card-title">Zapatilla</h3>
+          <section className="card-reviews">
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <span className="total-reviews"> 4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>
+              $200
+            </div>
+            <div className="bag">
+              <IoBagAddSharp className="bag-icon"/>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <section className="card">
+        <img src="https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg"
+             alt="Shoe"
+             className="card-img"/>
+        <div className="card-details">
+          <h3 className="card-title">Zapatilla</h3>
+          <section className="card-reviews">
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <AiFillStar className="ratings-star"/>
+            <span className="total-reviews"> 4</span>
+          </section>
+          <section className="card-price">
+            <div className="price"> $200
+            </div>
+            <div className="bag">
+              <IoBagAddSharp className="bag-icon"/>
+            </div>
           </section>
         </div>
       </section>
