4981c83c39a70610c363949b2d0a0b2fa07dbbc1
Facundo Martín Mayordomo <85538820+Facu-May@users.noreply.github.com>
2024-11-01 08:49:31 -0300

    Update productoService.js

--- src/services/productoService.js
+++ src/services/productoService.js
@@ -1,8 +1,72 @@
 import axios from "axios";
 import { API_URL } from "../utils";
 
-export async function getProductos(page, rowsPerPage)
+//Nota: debo renderizar los errores en la vista
+export async function getProducts() 
 {
-    let response = await axios.get(`${API_URL}/producto`)
-    return response.data
+    try 
+    {
+        const response = await axios.get(`${API_URL}/productos`);
+        return response.data;
+    } 
+    catch (error) 
+    {
+        console.error("Error al obtener los productos:", error);
+        throw error;
+    }
+}
+
+export async function addProduct(newProduct) 
+{
+    try 
+    {
+        const response = await axios.post(`${API_URL}/productos`, newProduct);
+        return response.data;
+    } 
+    catch (error) 
+    {
+        console.error("Error al agregar el producto:", error);
+        throw error; 
+    }
+}
+
+export async function editProduct(updatedProduct)
+{
+    try 
+    {
+        const response = await axios.put(`${API_URL}/productos/${updatedProduct.id}`, updatedProduct);
+        return response.data;
+    } 
+    catch (error) 
+    {
+        console.error("Error al editar el producto:", error);
+        throw error;
+    }
+}
+
+export async function deleteProduct(productId)
+{
+    try 
+    {
+        await axios.delete(`${API_URL}/productos/${productId}`);
+    } 
+    catch (error) 
+    {
+        console.error("Error al eliminar el producto:", error);
+        throw error;
+    }
+}
+
+export async function getProductById(productId)
+{
+    try 
+    {
+        const response = await axios.get(`${API_URL}/productos/${productId}`);
+        return response.data;
+    } 
+    catch (error) 
+    {
+        console.error("Error al obtener el producto:", error);
+        throw error;
+    }
 }
