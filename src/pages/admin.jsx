4d91239ba48c921b4a3291a287a4a71e76bec5c9
Facundo Martín Mayordomo <85538820+Facu-May@users.noreply.github.com>
2024-11-09 09:02:37 -0300

    Manejo de stock al agregar y editar producto
    Se agregó la posibilidad de agregar y modificar stock de productos de una manera más limpia y prolija.
    Próximo a agregar: debo agregar la parte de "img" en db.json con imágenes posiblemente de amazon o de internet, como en el data.json, para invocarlas después en el dashboard de productos.

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -31,6 +31,8 @@ export const ProductDashboard = () => {
     const [selectedProduct, setSelectedProduct] = useState(null);
     const [stockModalOpen, setStockModalOpen] = useState(false);
     const [selectedProductStock, setSelectedProductStock] = useState(null);
+    // Manejo del stock en los modals, descomentar esto si funciona
+    const [stock, setStock] = useState([]);
 
     // Obtener la lista de productos usando el servicio
     const fetchProducts = async () => {
@@ -47,12 +49,28 @@ export const ProductDashboard = () => {
         fetchProducts();
     }, []);
 
+    // Manejo del stock en los modals, descomentar esto si funciona
+    const handleStockChange = (index, field, value) => {
+        const updatedStock = [...stock];
+        updatedStock[index][field] = value;
+        setStock(updatedStock);
+    };
+
+    const addStockRow = () => {
+        setStock([...stock, { id: Date.now(), talle: '', cantidad: 0 }]);
+    };
+
+    const removeStockRow = (index) => {
+        setStock(stock.filter((_, i) => i !== index));
+    };
+
     // Agregar un nuevo producto
     const handleAddProduct = async (newProduct) => {
         try {
             const data = await addProduct(newProduct);
             setProducts([...products, data]);
             setOpenAddModal(false);
+            setStock([]);
         } catch (error) {
             console.error('Error al agregar el producto:', error);
             // Manejar el error
@@ -66,6 +84,7 @@ export const ProductDashboard = () => {
             setProducts(products.map(p => (p.id === data.id ? data : p)));
             setOpenEditModal(false);
             setSelectedProduct(null);
+            setStock([]);
         } catch (error) {
             console.error('Error al editar el producto:', error);
             // Manejar el error
@@ -153,14 +172,35 @@ export const ProductDashboard = () => {
                             nombre: e.target.nombre.value,
                             descripcion: e.target.descripcion.value,
                             precio: parseFloat(e.target.precio.value),
-                            stock: parseInt(e.target.stock.value),
+                            stock: stock,
                         };
                         handleAddProduct(newProduct);
                     }}>
                         <TextField label="Nombre" name="nombre" fullWidth margin="normal" />
                         <TextField label="Descripción" name="descripcion" fullWidth margin="normal" />
                         <TextField label="Precio" name="precio" type="number" fullWidth margin="normal" />
-                        <TextField label="Stock" name="stock" type="number" fullWidth margin="normal" />
+                        {/* Manejo del stock en los modals, descomentar esto si funciona */}
+                        <h3>Stock</h3>
+                        {stock.map((item, index) => (
+                            <Box key={item.id} display="flex" alignItems="center" mb={2}>
+                                <TextField
+                                    label="Talle"
+                                    type="number"
+                                    value={item.talle}
+                                    onChange={(e) => handleStockChange(index, 'talle', e.target.value)}
+                                    sx={{ marginRight: 2 }}
+                                />
+                                <TextField
+                                    label="Cantidad"
+                                    type="number"
+                                    value={item.cantidad}
+                                    onChange={(e) => handleStockChange(index, 'cantidad', e.target.value)}
+                                    sx={{ marginRight: 2 }}
+                                />
+                                <Button onClick={() => removeStockRow(index)} color="error">Eliminar</Button>
+                            </Box>
+                        ))}
+                        <Button onClick={addStockRow} color="primary">Agregar Stock</Button>
                         <Button type="submit" variant="contained">
                             Guardar
                         </Button>
@@ -190,14 +230,36 @@ export const ProductDashboard = () => {
                                 nombre: e.target.nombre.value,
                                 descripcion: e.target.descripcion.value,
                                 precio: parseFloat(e.target.precio.value),
-                                stock: parseInt(e.target.stock.value),
+                                stock: stock, // Usamos el estado `stock` que contiene las modificaciones
                             };
                             handleEditProduct(updatedProduct);
                         }}>
                             <TextField label="Nombre" name="nombre" fullWidth margin="normal" defaultValue={selectedProduct.nombre} />
                             <TextField label="Descripción" name="descripcion" fullWidth margin="normal" defaultValue={selectedProduct.descripcion} />
                             <TextField label="Precio" name="precio" type="number" fullWidth margin="normal" defaultValue={selectedProduct.precio} />
-                            <TextField label="Stock" name="stock" type="number" fullWidth margin="normal" defaultValue={selectedProduct.stock} />
+                            {/* <TextField label="Stock" name="stock" type="number" fullWidth margin="normal" defaultValue={selectedProduct.stock} /> */}
+                            {/* Manejo del stock en los modals, descomentar esto si funciona */}
+                            <h3>Stock</h3>
+                            {stock.map((item, index) => (
+                                <Box key={item.id} display="flex" alignItems="center" mb={2}>
+                                    <TextField
+                                        label="Talle"
+                                        type="number"
+                                        value={item.talle}
+                                        onChange={(e) => handleStockChange(index, 'talle', e.target.value)}
+                                        sx={{ marginRight: 2 }}
+                                    />
+                                    <TextField
+                                        label="Cantidad"
+                                        type="number"
+                                        value={item.cantidad}
+                                        onChange={(e) => handleStockChange(index, 'cantidad', e.target.value)}
+                                        sx={{ marginRight: 2 }}
+                                    />
+                                    <Button onClick={() => removeStockRow(index)} color="error">Eliminar</Button>
+                                </Box>
+                            ))}
+                            <Button onClick={addStockRow} color="primary">Agregar Stock</Button>
                             <Button type="submit" variant="contained">
                                 Guardar
                             </Button>
